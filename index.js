237073789a8cf802c479211d061b06c27631afa4
Elaine <elainefelipemoreira@gmail.com>
2022-02-14 19:54:09 -0300

    Cria rota GET para novelas

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const novelas = require('./src/database/novelas.json');
 
 const app = express();
 
 const PORT = 3000;
+
+app.get('/novelas', (req, res) => {
+  return res.json(novelas)
+});
 
 app.listen(PORT, () => console.log(`Online na porta ${PORT}`));
